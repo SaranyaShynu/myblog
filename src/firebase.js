bec2040363ead7e136241e4d3647d0d6c76d257a
SaranyaShynu <saranyarajendran790@gmail.com>
2026-01-05 13:03:36 +0530

    new blog

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-SRVP-J9ziVxASEQjYFavyyVie1i5Hzs",
-  authDomain: "blog-react-65fa9.firebaseapp.com",
-  projectId: "blog-react-65fa9",
-  storageBucket: "blog-react-65fa9.firebasestorage.app",
-  messagingSenderId: "988003212432",
-  appId: "1:988003212432:web:dba0e62274790751b1cc15",
-  measurementId: "G-2JT11JJGRW"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
